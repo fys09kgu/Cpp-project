b6a49456284a8b3017be101431fdb7e3654d8b57
Erik Stenlund <zba10est@varg-3.student.lth.se>
2015-03-09 17:04:37 +0100

    messagehandler

---
+++ client/messagehandler.cc
@@ -0,0 +1,75 @@
+#include <string>
+#include "connection.h"
+#include "protocol.h"
+#include "messagehandler.h"
+
+MessageHandler::MessageHandler(std::shared_ptr<Connection> conn_ptr) : conn(conn_ptr) {
+	
+}
+
+void MessageHandler::sendCode(int code) {
+	sendByte(code);
+}
+
+void MessageHandler::sendIntParameter(int parameter) {
+	sendCode(Protocol::PAR_NUM);
+	sendInt(parameter);
+}
+
+void MessageHandler::sendStringParameter(std::string parameter){
+	sendCode(Protocol::PAR_STRING);
+	sendInt(parameter.length());
+	for(auto it = parameter.begin(); it != parameter.end(); ++it) {
+		sendByte(*it);
+	}
+}
+
+int MessageHandler::recvCode() {
+	return recvByte();
+}
+
+int MessageHandler::recvInt() {
+	int b1 = recvByte();
+	int b2 = recvByte();
+	int b3 = recvByte();
+	int b4 = recvByte();
+
+	return b1 << 24 | b2 << 16 | b3 << 8 | b4;
+}
+
+//Maybe throw exception
+int MessageHandler::recvIntParameters() {
+		//Check code??
+	int code = recvCode();
+	return recvInt();	
+}
+
+//throw excep?
+std::string MessageHandler::recvStringParameter() {
+
+	//Check code??
+	int code = recvCode();
+	int n = recvInt();
+	std::string parameter = "";
+	for (int i = 0; i < n; i++) {
+		parameter += conn->read();
+	}
+
+	return parameter;
+}
+
+
+void MessageHandler::sendInt(int value) {
+		sendByte((value >> 24) & 0xFF);
+		sendByte((value >> 16) & 0xFF);
+		sendByte((value >> 8) & 0xFF);
+		sendByte(value & 0xFF);	
+}
+
+int MessageHandler::recvByte() {
+	/*read returnerar unsigned char*/
+	return (int) conn->read();
+}
+void MessageHandler::sendByte(char code) {
+	conn->write((char) code);
+}

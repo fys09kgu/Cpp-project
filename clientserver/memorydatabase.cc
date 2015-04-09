6f9e270bb126018d9d7bb7e84fc3b78580f0b769
dat11aka <dat11aka@.student.lu.se>
2015-04-09 13:19:30 +0200

    Kommentarer till memorydatabase.cc

--- clientserver/memorydatabase.cc
+++ clientserver/memorydatabase.cc
@@ -4,20 +4,11 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-
-	MemoryDatabase::MemoryDatabase() {
 	
-	}
-/*
-	void MemoryDatabase::incID(){
-		++nextID;	
-	}
-*/
-/*	Newsgroup MemoryDatabase::getNewsgroup(uint id){
-		return *newsgroups.find(id);
-	}
-*/
+	// Constructor of memorydatabase
+	MemoryDatabase::MemoryDatabase() {}
+
+	// Add new newsgroup with title
 	bool MemoryDatabase::addNewsgroup(string title){
 		Newsgroup gr;
 		gr.title = title;
@@ -27,18 +18,12 @@ using namespace std;
 				return false;
 			}
 		}
-/*
-		for_each(newsgroups.begin(), newsgroups.end(), [&](pair<const uint, Newsgroup>& grp){
-			if(gr.title == grp.second.title){
-				return false;
-			}	
-		}); */
 		newsgroups.insert(make_pair(getID(), gr));
 		incID();
 		return true;
-		
 	}
 
+	// Returns a pointer to newsgroup with ID
 	Newsgroup* MemoryDatabase::getNewsgroup(uint ID){
 		Newsgroup* ptr = nullptr;
 		if(newsgroups.count(ID) != 0){
@@ -47,14 +32,14 @@ using namespace std;
 		return ptr;
 	}
 
-
+	//Add article with title, author and text in a newsgroup with ID newsgroupID,
+	// returns true if article successfully was added. 
 	bool MemoryDatabase::addArticle(uint newsgroupID, string title, string author, string text){
 			Article ar;	
 			ar.title = title;
 			ar.author = author;
 			ar.text = text;
 			
-//			if(newsgroups[newsgroupID].articles.insert(make_pair(newsgroups[newsgroupID].nextArtID, ar)).second){
 			if(getNewsgroup(newsgroupID) != nullptr){
 				newsgroups[newsgroupID].articles.insert(make_pair(newsgroups[newsgroupID].nextArtID, ar));
 				++newsgroups[newsgroupID].nextArtID;
@@ -62,23 +47,28 @@ using namespace std;
 			}
 			return false;
 	}
-
+	
+	// Remove newsgroup, returns true if it was successfully removed.
 	bool MemoryDatabase::removeNewsgroup(uint newsgroupID){
 		return	newsgroups.erase(newsgroupID);
 	}
 
+	// Remove article, returns true if it was successfully removed.
 	bool MemoryDatabase::removeArticle(uint newsgroupID, uint articleID){
 		return newsgroups[newsgroupID].articles.erase(articleID);
 	}
-
+	
+	// Returns newsgroups
 	std::map<uint, Newsgroup> MemoryDatabase::getNewsgroups(){
 		return newsgroups;
 	}
-
+	
+	// Returns the number of newsgroups.
 	uint MemoryDatabase::getNewsgroupCount() {
 		return newsgroups.size();
 	}
 
+	// Check if an article exists.
 	bool MemoryDatabase::articleExists(uint newsgroupID, uint articleID) {
 		return newsgroups[newsgroupID].articles.count(articleID) != 0;
 	}
@@ -86,14 +76,4 @@ using namespace std;
 	Article MemoryDatabase::getArticle(uint newsgroupID, uint articleID) {
 		return newsgroups[newsgroupID].articles[articleID];
 	}
-	
-
-
-
-
-
-
-
-
-	
 
